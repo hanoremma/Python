b4e7ee1da2608e73fd44961a092436abc163bccb
Emma's Page <emmahalimah049@gmail.com>
2025-02-03 20:26:50 +0700

    Perubahan k-2

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print ("Hello")
+# Program memeriksa ganjil genap
+angka = int(input("Masukkan sebuah angka: "))
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan genap.")
+else:
+    print(f"{angka} adalah bilangan ganjil.")
